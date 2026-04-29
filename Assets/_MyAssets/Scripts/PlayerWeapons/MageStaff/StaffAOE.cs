d784e25f425fad56be1527ecd6569282070d5149
XavierDam <xdassass1n22@gmail.com>
2026-04-29 10:29:07 -0400

    Staff joueur

---
+++ Assets/_MyAssets/Scripts/PlayerWeapons/MageStaff/StaffAOE.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class StaffAOE : MonoBehaviour
+{
+    [SerializeField] private float lifeTime = 7f;
+
+    private void Start()
+    {
+        Destroy(gameObject, lifeTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
