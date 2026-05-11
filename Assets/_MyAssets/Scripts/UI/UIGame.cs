f63cfb0139f480bb4b5fd96ca558f766631a51ee
JeremyIsa <jeremy.isabelle257@outlook.com>
2026-05-11 17:05:16 -0400

    update

--- Assets/_MyAssets/Scripts/UI/UIGame.cs
+++ Assets/_MyAssets/Scripts/UI/UIGame.cs
@@ -5,6 +5,7 @@ using UnityEngine.EventSystems;
 
 public class UIGame : UI
 {
+
     public static UIGame Instance;
 
     [SerializeField] private TextMeshProUGUI _txtLevel;
@@ -13,6 +14,7 @@ public class UIGame : UI
     [SerializeField] private GameObject _instructionsPanel;
     [SerializeField] private GameObject _gameBar;
     [SerializeField] private Button _buttonCloseInstructions;
+    [SerializeField] private Button _buttonFirstUpgrade;
 
     [Header("Barres UI")]
     [SerializeField] private Image _lifeBar;
@@ -40,37 +42,48 @@ public class UIGame : UI
 
     private void Start()
     {
+
         _currentLife = _maxLife;
-        _lifeBar.fillAmount = 1f;  // plein
-        _xpBar.fillAmount = 0f;    // vide
+        _lifeBar.fillAmount = 1f;
+        _xpBar.fillAmount = 0f;    
 
         if (GameManager.Instance != null)
             GameManager.Instance.OnEnemyDestroyed += OnEnemyDestroyed;
 
         Time.timeScale = 0f;
+
         _instructionsPanel.SetActive(true);
         _gameBar.SetActive(false);
         _upgradePanel.SetActive(false);
+
         EventSystem.current.SetSelectedGameObject(_buttonCloseInstructions.gameObject);
+
         _txtLevel.text = $"Niveau {level}";
         _txtPoints.text = $"{points}";
+
     }
 
     private void OnDestroy()
     {
+
         if (GameManager.Instance != null)
             GameManager.Instance.OnEnemyDestroyed -= OnEnemyDestroyed;
+
     }
 
     private void OnEnemyDestroyed(object sender, GameManager.OnEnemyDestroyedEventArgs e)
     {
+
         if (e.DestroyedObjectTag == "Player")
         {
+
             _currentLife = Mathf.Max(0, _currentLife - 1f);
             _lifeBar.fillAmount = 1f - (_currentLife / _maxLife);
+
         }
         else if (e.DestroyedObjectTag == "PlayerAttack")
         {
+
             _enemiesKilled++;
             points++;
             _txtPoints.text = $"{points}";
@@ -78,32 +91,47 @@ public class UIGame : UI
 
             if (_enemiesKilled % _enemiesPerLevel == 0)
                 OpenUpgradePanel();
+
         }
+
     }
 
     public void OnCloseClick()
     {
+
         Time.timeScale = 1.0f;
+
         _instructionsPanel.SetActive(false);
         _upgradePanel.SetActive(false);
         _gameBar.SetActive(true);
+
     }
 
     public void OpenUpgradePanel()
     {
+
         Time.timeScale = 0f;
+
         _upgradePanel.SetActive(true);
         _gameBar.SetActive(false);
         _instructionsPanel.SetActive(false);
+
+        EventSystem.current.SetSelectedGameObject(_buttonFirstUpgrade.gameObject);
+
     }
 
     public void UpgradeChosen()
     {
+
         Time.timeScale = 1.0f;
+
         _instructionsPanel.SetActive(false);
         _upgradePanel.SetActive(false);
         _gameBar.SetActive(true);
+
         level += 1;
         _txtLevel.text = $"Niveau {level}";
+
     }
+
 }
